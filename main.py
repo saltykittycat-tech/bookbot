371f5e55d52cf0ca6c6f720cc24859af10792044
saltykittycat-tech <saltykittycat@gmail.com>
2025-08-24 16:27:58 -0500

    finished project

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from stats import word_count
 from stats import get_character_count
 from stats import dictionary_to_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-book_text = get_book_text("books/frankenstein.txt")
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
+
+book_path = sys.argv[1]
+book_text = get_book_text(book_path)
 character_count = get_character_count(book_text)
 character_list = dictionary_to_list(character_count)
 print("=============== BOOKBOT ===============")
-print("Analyzing book found at books/frankenstein.txt")
+print(f"Analyzing book found at {book_path}")
 print("---------- Word Count ---------")
 word_count(book_text)
 print("----------- Character Count ------------")
