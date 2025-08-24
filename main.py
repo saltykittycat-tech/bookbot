dae3aec689348286dcc20bfd83980396facd7939
saltykittycat-tech <saltykittycat@gmail.com>
2025-08-24 15:30:44 -0500

    dictionary of characters and their counts added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import get_character_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,5 +7,7 @@ def get_book_text(filepath):
     return file_contents
 
 book_text = get_book_text("books/frankenstein.txt")
+character_count = get_character_count(book_text)
 
 word_count(book_text)
+print(character_count)
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def word_count(book_text):
     word_list = book_text.split()
     word_count = len(word_list)
     print(f"{word_count} words found in the document")
+
+def get_character_count(book_text):
+    character_count={}
+    for character in book_text:
+        character = character.lower()
+        if character in character_count:
+            character_amount = character_count[character]
+            character_amount += 1
+            character_count[character] = character_amount
+        else:
+            character_count[character] = 1
+    return character_count
